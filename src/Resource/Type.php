265088979629dc9cebc23d47eabdb297e874f8a6
Vinícius de Oliveira Souza <viniciuso970@gmail.com>
2024-08-12 20:36:36 -0300

    Fixing stat sync.

--- src/Resource/Type.php
+++ src/Resource/Type.php
@@ -10,25 +10,11 @@ use Drupal\pokemon_api\Translation;
 class Type extends Resource {
 
   /**
-   * The moves.
-   *
-   * @var array|null
-   */
-  private array|null $moves;
-
-  /**
    * The names.
    *
    * @var \Drupal\pokemon_api\Translation|null
    */
   private Translation|null $names;
-
-  /**
-   * The pokemon.
-   *
-   * @var array|null
-   */
-  private array|null $pokemon;
 
   /**
    * The endpoint.
@@ -45,26 +31,6 @@ class Type extends Resource {
    */
   public static function getEndpoint(): string {
     return self::ENDPOINT;
-  }
-
-  /**
-   * Get the moves.
-   *
-   * @return array
-   *   The moves.
-   */
-  public function getMoves(): array {
-    return $this->moves;
-  }
-
-  /**
-   * Set the moves.
-   *
-   * @param array $moves
-   *   The moves.
-   */
-  public function setMoves($moves) {
-    $this->moves = $moves;
   }
 
   /**
@@ -85,26 +51,6 @@ class Type extends Resource {
    */
   public function setNames(array $names) {
     $this->names = new Translation($names);
-  }
-
-  /**
-   * Get the pokemon.
-   *
-   * @return array
-   *   The pokemon.
-   */
-  public function getPokemon(): array {
-    return $this->pokemon;
-  }
-
-  /**
-   * Set the pokemon.
-   *
-   * @param array $pokemon
-   *   The pokemon.
-   */
-  public function setPokemon(array $pokemon) {
-    $this->pokemon = $pokemon;
   }
 
 }
