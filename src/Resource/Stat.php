588669a409d11eb614ed636eb2c835c2a6de9ca9
Vinícius de Oliveira Souza <viniciuso970@gmail.com>
2024-08-12 20:24:15 -0300

    Adding stat sync.

---
+++ src/Resource/Stat.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\pokemon_api\Resource;
+
+use Drupal\pokemon_api\Translation;
+
+/**
+ * Resource Stat class.
+ */
+class Stat extends Resource {
+
+  /**
+   * The names.
+   *
+   * @var \Drupal\pokemon_api\Translation|null
+   */
+  private Translation|null $names;
+
+  /**
+   * The endpoint.
+   *
+   * @var string
+   */
+  private const ENDPOINT = 'stat';
+
+  /**
+   * Get the endpoint.
+   *
+   * @return string
+   *   The endpoint.
+   */
+  public static function getEndpoint(): string {
+    return self::ENDPOINT;
+  }
+
+  /**
+   * Get the names.
+   *
+   * @return \Drupal\pokemon_api\Translation|null
+   *   The names.
+   */
+  public function getNames(): ?Translation {
+    return $this->names;
+  }
+
+  /**
+   * Set the names.
+   *
+   * @param array $names
+   *   The names.
+   */
+  public function setNames(array $names) {
+    $this->names = new Translation($names);
+  }
+
+}
